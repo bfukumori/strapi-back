0fc9f0b1010280cbaf34fe927f70519f41efe1bb
Bruno Fukumori <brunofukumori@gmail.com>
2025-04-10 01:16:52 -0300

    :tada: initial commit

---
+++ src/api/wefiter/controllers/wefiter.ts
@@ -0,0 +1,7 @@
+/**
+ * wefiter controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::wefiter.wefiter');
---
+++ src/api/wefiter/routes/wefiter.ts
@@ -0,0 +1,7 @@
+/**
+ * wefiter router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::wefiter.wefiter');
